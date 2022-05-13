7025079db1dea0b53cbb114e8987aa9fbaf2133c
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-13 23:59:32 +0200

    snake is eating the apples but not yet growing

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 
+const player = document.getElementById("ReadyPlayerOne");
+const control = document.getElementById('canvas').getContext('2d');
 
 // var viewportWidth = 635;
 // var viewportHeight = 1385;
@@ -19,17 +21,17 @@ let randomApple3 = generateApple();
 let randomApple4 = generateApple();
 let randomApple5 = generateApple();
 // define all boundaries in new array with values of (x1,y1,x2,y2)
-arrAppleBoundaries.push(randomApple[0],  randomApple[1],  randomApple[2],  randomApple[3]);
-arrAppleBoundaries.push(randomApple2[0], randomApple2[1], randomApple2[2], randomApple2[3]);
-arrAppleBoundaries.push(randomApple3[0], randomApple3[1], randomApple3[2], randomApple3[3]);
-arrAppleBoundaries.push(randomApple4[0], randomApple4[1], randomApple4[2], randomApple4[3]);
-arrAppleBoundaries.push(randomApple5[0], randomApple5[1], randomApple5[2], randomApple5[3]);
+arrAppleBoundaries.push([randomApple[0],  randomApple[1],  randomApple[2],  randomApple[3]]);
+arrAppleBoundaries.push([randomApple2[0], randomApple2[1], randomApple2[2], randomApple2[3]]);
+arrAppleBoundaries.push([randomApple3[0], randomApple3[1], randomApple3[2], randomApple3[3]]);
+arrAppleBoundaries.push([randomApple4[0], randomApple4[1], randomApple4[2], randomApple4[3]]);
+arrAppleBoundaries.push([randomApple5[0], randomApple5[1], randomApple5[2], randomApple5[3]]);
 
 // console.log("apples is " + arrAppleBoundaries.length);
-console.log(arrAppleBoundaries);
-for (let i = 0; i < arrAppleBoundaries.length; i++) {
-    console.log("apple (" + (i+1) + ") (x1,y1): (" + arrAppleBoundaries[i][0] + "," + arrAppleBoundaries[i][1] + ") (x2,y2): (" + arrAppleBoundaries[i][2] + "," + arrAppleBoundaries[i][3] + ")");
-}
+// console.log(arrAppleBoundaries);
+// for (let i = 0; i < arrAppleBoundaries.length; i++) {
+//     console.log("apple (" + (i+1) + ") (x1,y1): (" + arrAppleBoundaries[i][0] + "," + arrAppleBoundaries[i][1] + ") (x2,y2): (" + arrAppleBoundaries[i][2] + "," + arrAppleBoundaries[i][3] + ")");
+// }
 
 
 window.addEventListener('resize',()=>{
@@ -37,7 +39,6 @@ window.addEventListener('resize',()=>{
     viewportHeight = document.documentElement.clientHeight-appleSize-appleSize;
 })
 
-const player = document.getElementById("ReadyPlayerOne");
 let x = viewportHeight/2;
 let y= viewportWidth/2;
 let direction;
@@ -68,10 +69,32 @@ function main(){
         y=y-IncreaseValue;
     }
     movePlayer(x, y);
+    getPlayerLocation();
     setTimeout(main, 50);
 }
 
 main();
+
+function getPlayerLocation(){
+    let top = player.style.top;
+    console.log("top: " + top);
+    let left = player.style.left;
+    console.log("left: " + left);
+    // let bottom = ;
+    for (let i = 0; i < arrAppleBoundaries.length; i++) {
+        if(x >= arrAppleBoundaries[i][0] && x <= arrAppleBoundaries[i][2]){
+            console.log(i + " boundaries are crossed!");
+            console.log(arrAppleBoundaries[i][0] + " x " + arrAppleBoundaries[i][2] + " " + x);
+            if(y >= arrAppleBoundaries[i][1] && y <= arrAppleBoundaries[i][3]){
+                //grow snake grow! for now console log what you want to do little snake.
+                console.log(i + " boundaries are crossed!");
+                console.log(arrAppleBoundaries[i][1] + " y " + arrAppleBoundaries[i][3] + " " + y);
+                control.clearRect(arrAppleBoundaries[i][0], arrAppleBoundaries[i][1], arrAppleBoundaries[i][2], arrAppleBoundaries[i][3]);
+
+            }
+        }
+    }
+}
 
 function movePlayer(x, y){
     // console.log(viewportHeight);
@@ -97,24 +120,11 @@ function movePlayer(x, y){
         y=maxViewportWidth-appleSize-IncreaseValue-IncreaseValue-IncreaseValue-5;
         direction = "";
     }
-    for (let i = 0; i < arrAppleBoundaries.length; i++) {
-        if(x === arrAppleBoundaries[i][0] || x===arrAppleBoundaries[i][2]){
-            console.log(arrAppleBoundaries[i][0] + " x " + arrAppleBoundaries[i][2]);
-            if(y === arrAppleBoundaries[i][1] || x === arrAppleBoundaries[i][3]){
-                console.log(arrAppleBoundaries[i][1] + " y " + arrAppleBoundaries[i][3]);
-                //grow snake grow! for now consolelog what you want to do little snake..
-                // what kind of monster feeds a snake apples by the way..
-                // console.log("boundaries are crossed!")
-
-            }
-        }
-    }
     moveXY.style.left = x + "px";
     moveXY.style.top  = y + "px";
 }
 
 function generateApple(){
-    const control = document.getElementById('canvas').getContext('2d');
     let rTwo = appleSize
     let r = rTwo / 2;
     let appleX = intRandom(r, 1360);
@@ -140,8 +150,8 @@ function generateApple(){
     // actually draw the boundaries fillRect(x, y, width plus a little extra, height plus a little extra);
     let appleRectX1 = appleX-r-3;
     let appleRectY1 = appleY-r-3;
-    let appleRectX2 = appleRectX1+appleSize+6;
-    let appleRectY2 = appleRectY1+appleSize+6;
+    let appleRectX2 = parseInt(appleRectX1) + appleSize + 6;
+    let appleRectY2 = parseInt(appleRectY1) + appleSize + 6;
     control.fillRect(appleRectX1, appleRectY1, appleSize+6, appleSize+6);
     // reset opacity for further drawing..
     control.globalAlpha = 1;
