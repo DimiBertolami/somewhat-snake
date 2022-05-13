95f8e1b658ccc6dfdefdbef54f049e75825c995a
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-13 15:22:58 +0200

    boundaries working again

--- game.js
+++ game.js
@@ -1,25 +1,27 @@
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext("2d");
-let r = canvas.parentElement.getBoundingClientRect();
-canvas.width = r.width;
-canvas.height = r.height;
-
-context.fillStyle = "red";
-context.fillRect(0, 0, canvas.width, canvas.height);
+// let canvas = document.getElementById('canvas');
+// let context = canvas.getContext("2d");
+// let r = canvas.parentElement.getBoundingClientRect();
+// canvas.width = r.width;
+// canvas.height = r.height;
+//
+// context.fillStyle = "red";
+// context.fillRect(0, 0, canvas.width, canvas.height);
 /*----GetWindowSizeNoScrollBarrs--START-------*/
 //Get inner width and height of current window
-var viewportWidth = document.documentElement.clientWidth;
-var viewportHeight = document.documentElement.clientHeight;
+// var viewportWidth = document.documentElement.clientWidth;
+// var viewportHeight = document.documentElement.clientHeight;
+var viewportWidth = 635;
+var viewportHeight = 1385;
 //Update values if window is resized
 window.addEventListener('resize',()=>{
     viewportWidth = document.documentElement.clientWidth;
-    viewportWidth = document.documentElement.clientHeight;
+    viewportHeight = document.documentElement.clientHeight;
 })
 /*----GetWindowSizeNoScrollBarrs--FINISH-------*/
 const player = document.getElementById("ReadyPlayerOne");
-let x = 300;
-let y= 300;
-let IncreaseValue = 20;
+let x = viewportHeight/2;
+let y= viewportWidth/2;
+let IncreaseValue = 10;
 let direction;
 
 document.body.addEventListener("keydown", function(event){
@@ -54,6 +56,10 @@ function main(){
 main();
 
 function movePlayer(x, y){
+    console.log(viewportHeight);
+    console.log(viewportWidth);
+    let maxViewportHeight = viewportHeight-50;
+    let maxViewportWidth = viewportWidth-45;
     let moveXY = document.getElementById("ReadyPlayerOne");
     console.log("x: " + x + " y: " + y);
     if(x<=7){
@@ -64,12 +70,12 @@ function movePlayer(x, y){
         y=7
         direction = "";
     }
-    if(x>=viewportHeight){
-        x=viewportHeight;
+    if(x>=maxViewportHeight){
+        x=maxViewportHeight;
         direction = "";
     }
-    if(y>=viewportWidth){
-        y=viewportWidth;
+    if(y>=maxViewportWidth){
+        y=maxViewportWidth;
         direction = "";
     }
     moveXY.style.left = x + "px";
