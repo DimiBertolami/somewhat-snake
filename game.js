7dc4cdbc7722e6ecd7e1e496dff2c0604b157fed
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-14 10:13:31 +0200

    snake can move again and eat apples. still full of bugs, occasionally you see enough apples even though the logic detects that there are no more apples so it spawns a bunch more apples. Will be my main focus to get the logic corrected.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-const player = document.getElementById("ReadyPlayerOne");
+// const player = document.getElementById("ReadyPlayerOne");
 const control = document.getElementById('canvas').getContext('2d');
 
 // var viewportWidth = 635;
@@ -7,14 +7,16 @@ const control = document.getElementById('canvas').getContext('2d');
 let viewportHeight = document.documentElement.clientWidth;
 let viewportWidth = document.documentElement.clientHeight;
 
-const IncreaseValue = 10;
+// TimeoutValue sets the refreshrate, now it's set to 10 times per second (milliseconds)
+const timeoutValue = 100;
+const IncreaseValue = 30;
 const appleSize = 30;
-var canvasEl = document.getElementsByTagName('canvas')[0];
+const canvasEl = document.getElementsByTagName('canvas')[0];
 canvasEl.width = viewportHeight-appleSize;
 canvasEl.height = viewportWidth-appleSize;
 
 
-CreateAppels();
+
 window.addEventListener('resize',()=>{
     viewportWidth = document.documentElement.clientWidth-appleSize-appleSize;
     viewportHeight = document.documentElement.clientHeight-appleSize-appleSize;
@@ -25,19 +27,27 @@ let y= viewportWidth/2;
 let direction;
 
 document.body.addEventListener("keydown", function(event){
-    direction = event.key;
-    // console.log(direction);
+        direction = event.key;
+        // console.log(direction);
     }
 );
 document.body.addEventListener("keyup", function(event){
-    direction = event.key;
+        direction = event.key;
     }
 )
+
+let arrAppleBoundaries = [];
+// this code generates the random apples
+arrAppleBoundaries.push(generateApple());
+arrAppleBoundaries.push(generateApple());
+arrAppleBoundaries.push(generateApple());
+arrAppleBoundaries.push(generateApple());
+arrAppleBoundaries.push(generateApple());
 
 
 function main(){
     if(direction === 'ArrowRight'){
-        x++;
+        // x++;
         x= x+IncreaseValue;
     }
     if(direction === 'ArrowLeft'){
@@ -50,32 +60,13 @@ function main(){
         y=y-IncreaseValue;
     }
     movePlayer(x, y);
-    getPlayerLocation();
-    setTimeout(main, 50);
+    setTimeout(main, timeoutValue);      //
 }
 
 main();
 
-function getPlayerLocation(){
-    // let top = player.style.top;
-    // let left = player.style.left;
-    for (let i = 0; i < arrAppleBoundaries.length; i++) {
-        if(x >= arrAppleBoundaries[i][0] && x <= arrAppleBoundaries[i][2]){
-            if(y >= arrAppleBoundaries[i][1] && y <= arrAppleBoundaries[i][3]){
-                //grow snake grow! for now console log what you want to do little snake.
-                control.clearRect(arrAppleBoundaries[i][0], arrAppleBoundaries[i][1], arrAppleBoundaries[i][2], arrAppleBoundaries[i][3]);
-                arrAppleBoundaries.splice(i, 1);
-            }
-        }
-    }
-    if(arrAppleBoundaries.length===0){
-        CreateAppels()
-    }
-}
-
 function movePlayer(x, y){
     // console.log(viewportHeight);
-    console.log("_________________________________________________________________________________");
     let maxViewportHeight = viewportHeight-appleSize-IncreaseValue;
     let maxViewportWidth = viewportWidth-appleSize-IncreaseValue;
     let moveXY = document.getElementById("ReadyPlayerOne");
@@ -97,6 +88,31 @@ function movePlayer(x, y){
         y=maxViewportWidth-appleSize-IncreaseValue-IncreaseValue-IncreaseValue-5;
         direction = "";
     }
+// define all boundaries in new array with values of (x1,y1,x2,y2)
+//     if(arrAppleBoundaries.isArray()){
+        if(arrAppleBoundaries.length===0){
+            // this code generates the random apples
+            arrAppleBoundaries.push(generateApple());
+            arrAppleBoundaries.push(generateApple());
+            arrAppleBoundaries.push(generateApple());
+            arrAppleBoundaries.push(generateApple());
+            arrAppleBoundaries.push(generateApple());
+            console.log(arrAppleBoundaries);
+        } else {
+            console.log("apples still on canvas: " + arrAppleBoundaries.length)
+        }
+    for (let i = 0; i < arrAppleBoundaries.length; i++) {
+        if(x >= arrAppleBoundaries[i][0] && x <= arrAppleBoundaries[i][2]){
+            if(y >= arrAppleBoundaries[i][1] && y <= arrAppleBoundaries[i][3]){
+                //grow snake grow! for now console log what you want to do little snake.
+                // control.beginPath();
+                control.clearRect(arrAppleBoundaries[i][0], arrAppleBoundaries[i][1], arrAppleBoundaries[i][2], arrAppleBoundaries[i][3]);
+                arrAppleBoundaries.splice(i);
+            }
+        }
+    }
+
+    // }
     moveXY.style.left = x + "px";
     moveXY.style.top  = y + "px";
 }
@@ -110,7 +126,7 @@ function generateApple(){
     control.fillStyle = '#FF0000';
     // begin drawing
     control.beginPath();
-    // circle (x, y, startAngle = 0, endAngle, boolCouterClockWise)
+    // circle (x, y, startAngle = 0, endAngle, boolCounterClockWise)
     control.arc(appleX, appleY, r, 0, 2 * Math.PI, false);
     // fill circle inside red
     control.fill();
@@ -121,9 +137,10 @@ function generateApple(){
     control.stroke();
     // draw green
     control.fillStyle = "green";
-    // half the opacity of the apple
-    control.globalAlpha = 0.5;
-    // actually draw the boundaries fillRect(x, y, width plus a little extra, height plus a little extra);
+    // almost half of the total opacity
+    control.globalAlpha = 0.4;
+    // draw the boundaries using rectangular overlay (to have "overlap-coordinates")
+    //        fillRect(x, y, width plus a little extra, height plus a little extra);
     let appleRectX1 = appleX-r-3;
     let appleRectY1 = appleY-r-3;
     let appleRectX2 = parseInt(appleRectX1) + appleSize + 6;
@@ -137,20 +154,4 @@ function generateApple(){
 
 function intRandom(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function CreateAppels(){
-    var arrAppleBoundaries = [];
-// this code generates the random apples
-    let randomApple = generateApple();
-    let randomApple2 = generateApple();
-    let randomApple3 = generateApple();
-    let randomApple4 = generateApple();
-    let randomApple5 = generateApple();
-// define all boundaries in new array with values of (x1,y1,x2,y2)
-    arrAppleBoundaries.push([randomApple[0],  randomApple[1],  randomApple[2],  randomApple[3]]);
-    arrAppleBoundaries.push([randomApple2[0], randomApple2[1], randomApple2[2], randomApple2[3]]);
-    arrAppleBoundaries.push([randomApple3[0], randomApple3[1], randomApple3[2], randomApple3[3]]);
-    arrAppleBoundaries.push([randomApple4[0], randomApple4[1], randomApple4[2], randomApple4[3]]);
-    arrAppleBoundaries.push([randomApple5[0], randomApple5[1], randomApple5[2], randomApple5[3]]);
 }
